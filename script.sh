e50dd41ac34605a5bd181a421bffadb0d53a209a
Thiago Locatelli <thiago.locatelli@gmail.com>
2019-10-10 19:11:33 -0400

    Added script

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/usr/local/bin/bash
+
+export BUILD_TYPE=$([[ ( -z "$BUILD_TYPE" ) && ( -f pom.xml ) ]] && echo JAVA_MAVEN || echo $BUILD_TYPE)
+
+export BUILD_TYPE=$([[ ( -z "$BUILD_TYPE" ) && ( -f build.gradle ) ]] && echo JAVA_GRADLE || echo $BUILD_TYPE)
+
+echo Build type':' $BUILD_TYPE
+
+
